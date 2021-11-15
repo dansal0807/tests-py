3759b48ce0122c1561c328bdbdcb99b01a4867ea
dansal0807 <danielsaldanha0@gmail.com>
2021-11-15 17:09:53 -0300

    First testing script.

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+from my_sum import sum
+
+#Criação da classe de testes herdando o unittest.
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
